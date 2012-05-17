c2d84cb7a152e739bcb02bbc71b39584681dfdc9
Eugene Kashida <eugene@cal.berkeley.edu>
2012-05-17 10:00:13 -0700

    add demo where cl is reinstantiated each time an iframe is created

---
+++ cl-memory-leak-investigation/cl/harness/dynamic/demo.js
@@ -0,0 +1,58 @@
+YUI({filter: 'debug', combine: false}).use('node', 'cl', function (Y) {
+
+    var body        = Y.one('body'),
+        start       = Y.one('#start'),
+        end         = Y.one('#end'),
+        url         = Y.one('#url'),
+        loads       = Y.one('#loads'),
+        auto        = Y.one('#automate'),
+        count       = 0,
+        numLoads,
+        automated,
+        iframe;
+
+    function create () {
+        Y.CL = new Y.CommunicationLayer();
+
+        iframe = Y.Node.create('<iframe>');
+        iframe.set('src', url.get('value'));
+
+        Y.CL.register(iframe, function (proxy) {
+            if (automated) {
+                proxy.ready(function () {
+                    destroy();
+                });
+            }
+        });
+
+        body.append(iframe);
+    }
+
+    function destroy () {
+        Y.CL.detachAll();
+
+        iframe.remove(true);
+        iframe = null;
+
+        if (console.markTimeline) {
+            console.markTimeline(++count);
+        }
+
+        if (automated && (numLoads && count < numLoads)) {
+            create();
+        }
+    }
+
+    start.on('click', function () {
+        automated = auto.get('checked');
+        numLoads = loads.get('value') || 0;
+
+        create();
+    });
+
+    end.on('click', function () {
+        automated = false;
+        destroy();
+    });
+
+});
